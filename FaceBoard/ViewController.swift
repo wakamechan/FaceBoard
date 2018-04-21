db09c534fdbbfe1cfc0a51a98fafb7d208994406
enomoto-r <ryohei@enomotoryouheinomacbook-air.local>
2018-04-21 15:14:28 +0900

    Initial Commit

---
+++ FaceBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceBoard
+//
+//  Created by 榎本亮平 on 2018/04/21.
+//  Copyright © 2018年 榎本亮平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FaceBoardTests/FaceBoardTests.swift
@@ -0,0 +1,36 @@
+//
+//  FaceBoardTests.swift
+//  FaceBoardTests
+//
+//  Created by 榎本亮平 on 2018/04/21.
+//  Copyright © 2018年 榎本亮平. All rights reserved.
+//
+
+import XCTest
+@testable import FaceBoard
+
+class FaceBoardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
